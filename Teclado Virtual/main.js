a0041ef50913e334587aa03ffe39ab05fa62fb6d
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2023-12-29 15:54:33 -0300

    teclado virtual

--- Teclado Virtual/main.js
+++ Teclado Virtual/main.js
@@ -1,3 +1,4 @@
+// Definición de las teclas en un arreglo bidimensional.
 const keys = [
   [
     ["1", "!"],
@@ -50,29 +51,34 @@ const keys = [
   [["SPACE", "SPACE"]],
 ];
 
+// Estados de las teclas especiales.
 let mayus = false;
 let shift = false;
 let current = null;
 
+// Función para renderizar el teclado en el DOM.
 function renderKeyboard() {
   const keyboard = document.querySelector("#keyboard-container");
   let empty = `<div class="key-empty"></div>`;
 
+  // Genera el HTML para cada capa del teclado.
   const layers = keys.map((layer) => {
     return layer.map((key) => {
+      // Manejo especial para teclas SHIFT, MAYUS y SPACE.
       if (key[0] === "SHIFT") {
         return `<button class="key key-shift ${shift ? "activated" : ""}">${
           key[0]
-        }</button>`; // Corregir aquí
+        }</button>`;
       }
       if (key[0] === "MAYUS") {
         return `<button class="key key-mayus ${mayus ? "activated" : ""}">${
           key[0]
-        }</button>`; // Corregir aquí
+        }</button>`;
       }
       if (key[0] === "SPACE") {
-        return `<button class="key key-space"></button>`; // Corregir aquí
+        return `<button class="key key-space"></button>`;
       }
+      // Para otras teclas normales.
       return `<button class="key key-normal">${
         shift
           ? key[1]
@@ -85,9 +91,11 @@ function renderKeyboard() {
     });
   });
 
+  // Añade una tecla vacía al final de la primera capa y al principio de la segunda.
   layers[0].push(empty);
   layers[1].unshift(empty);
 
+  // Combina las capas generadas en un solo HTML.
   const htmlLayers = layers.map((layer) => {
     return layer.join("");
   });
@@ -101,6 +109,7 @@ function renderKeyboard() {
   document.querySelectorAll(".key").forEach((key) => {
     key.addEventListener("click", (e) => {
       if (current) {
+        // Maneja acciones según la tecla presionada.
         if (key.textContent === "SHIFT") {
           shift = !shift;
         } else if (key.textContent === "MAYUS") {
@@ -120,10 +129,12 @@ function renderKeyboard() {
   });
 }
 
+// Asigna eventos de foco a los inputs para rastrear el elemento actual.
 document.querySelectorAll("input").forEach((input) => {
   input.addEventListener("focusin", (e) => {
     current = e.target;
   });
 });
 
+// Inicializa la renderización del teclado.
 renderKeyboard();
